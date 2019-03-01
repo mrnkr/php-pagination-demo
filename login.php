5dff95936ef135e1673ee3850b2fe538e69213a6
mrnkr <xmr.nkr@gmail.com>
2019-03-01 03:12:33 +0000

    Login almost ready - BUGS ON SIGHT

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php
+
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+
+require_once dirname(__FILE__) . '/src/utils.php';
+require_once dirname(__FILE__) . '/src/config/database.php';
+require_once dirname(__FILE__) . '/src/models/user.php';
+
+if (!isset($_POST['email']) || !isset($_POST['password'])) {
+  die();
+}
+
+session_start();
+
+$db   = new Database();
+$conn = $db->connect();
+
+$userModel = new User($conn);
+$user = $userModel->verify($_POST['email'], $_POST['password']);
+
+if (!empty($user)) {
+  $_SESSION['user'] = $user;
+  header('Location: https://192.168.1.10/users_2.php', true, 301);
+  exit();
+} else {
+  echo '<p>Usuario inexistente - <a href="https://192.168.1.10">volver...</a></p>';
+}
+
+?>
--- src/models/user.php
+++ src/models/user.php
@@ -1,20 +1,22 @@
 <?php
 
-require_once dirname(__FILE__) . '/../../utils.php';
-
 class User {
 
-  private $table = 'users';
+  private $table = 'affiliate';
   private $conn;
 
   public function __construct($db) {
     $this->conn = $db;
   }
 
-  public function findOne($id) {
-    $query = 'SELECT * FROM '. $this->table . ' WHERE id = :id';
+  public function verify($email, $password) {
+    // emails are set to be unique so there is no need to limit to one row
+    $query = 'SELECT * FROM ' . $this->table . ' WHERE email = :email AND password = :password';
     $stmt  = $this->conn->prepare($query);
-    $stmt->bindParam(':id', $limit);
+
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':password', md5($password));
+
     $stmt->execute();
 
     if ($stmt->rowCount() == 0) {
@@ -32,131 +34,8 @@ class User {
       'address' => $address,
       'phone' => $phone,
       'email' => $email,
-      'picture_url' => $picture_url,
-      'scope' => $scope
+      'picture_url' => $picture_url
     );
-  }
-
-  public function find($limit, $offset = 0) {
-    $limit = $limit - $offset;
-
-    $res = array(
-      'count' => 0,
-      'data' => array()
-    );
-
-    $query = 'SELECT * FROM ' . $this->table . ' LIMIT :limit OFFSET :offset';
-    $stmt  = $this->conn->prepare($query);
-    $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
-    $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
-    $stmt->execute();
-
-    $res['count'] = $stmt->rowCount();
-    
-    if ($res['count'] == 0) {
-      return $res;
-    }
-
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-      extract($row);
-      
-      $cur = array(
-        'id' => $id,
-        'first_name' => $first_name,
-        'last_name' => $last_name,
-        'birthdate' => $birthdate,
-        'address' => $address,
-        'phone' => $phone,
-        'email' => $email,
-        'picture_url' => $picture_url,
-        'scope' => $scope
-      );
-
-      array_push($res['data'], $cur);
-    }
-
-    return $res;
-  }
-
-  public function insertOne($user) {
-    $query = 'INSERT INTO ' . $this->table . '
-              SET first_name = :first_name,
-                  last_name = :last_name,
-                  birthdate = :birthdate,
-                  address = :address,
-                  phone = :phone,
-                  email = :email,
-                  picture_url = :picture_url,
-                  password = :password,
-                  scope = :scope';
-
-    $stmt = $this->conn->prepare($query);
-
-    $stmt->bindParam(':first_name', htmlspecialchars(strip_tags($user['first_name'])));
-    $stmt->bindParam(':last_name', htmlspecialchars(strip_tags($user['last_name'])));
-    $stmt->bindParam(':birthdate', htmlspecialchars(strip_tags($user['birthdate'])));
-    $stmt->bindParam(':address', htmlspecialchars(strip_tags($user['address'])));
-    $stmt->bindParam(':phone', htmlspecialchars(strip_tags($user['phone'])));
-    $stmt->bindParam(':email', htmlspecialchars(strip_tags($user['email'])));
-    $stmt->bindParam(':picture_url', htmlspecialchars(strip_tags($user['picture_url'])));
-    $stmt->bindParam(':password', md5($user['password']));
-    $stmt->bindParam(':scope', htmlspecialchars(strip_tags($user['scope'])));
-
-    if ($stmt->execute()) {
-      return $this->conn->lastInsertId();
-    }
-
-    return -1;
-  }
-
-  public function updateOne($id, $diff) {
-    $old = $this->findOne($id);
-
-    $query = 'UPDATE \`' . $this->table . '\`
-              SET first_name = :first_name,
-                  last_name = :last_name,
-                  birthdate = :birthdate,
-                  address = :address,
-                  phone = :phone,
-                  email = :email,
-                  picture_url = :picture_url,
-                  password = :password,
-                  scope = :scope
-              WHERE id = :id';
-
-    $stmt = $this->conn->prepare($query);
-
-    $stmt->bindParam(':first_name', isset($diff['first_name']) ? htmlspecialchars(strip_tags($diff['first_name'])) : $old['first_name']);
-    $stmt->bindParam(':last_name', isset($diff['last_name']) ? htmlspecialchars(strip_tags($diff['last_name'])) : $old['last_name']);
-    $stmt->bindParam(':birthdate', isset($diff['birthdate']) ? htmlspecialchars(strip_tags($diff['birthdate'])) : $old['birthdate']);
-    $stmt->bindParam(':address', isset($diff['address']) ? htmlspecialchars(strip_tags($diff['address'])) : $old['address']);
-    $stmt->bindParam(':phone', isset($diff['phone']) ? htmlspecialchars(strip_tags($diff['phone'])) : $old['phone']);
-    $stmt->bindParam(':email', isset($diff['email']) ? htmlspecialchars(strip_tags($diff['email'])) : $old['email']);
-    $stmt->bindParam(':picture_url', isset($diff['picture_url']) ? htmlspecialchars(strip_tags($diff['picture_url'])) : $old['picture_url']);
-    $stmt->bindParam(':password', isset($diff['password']) ? md5($diff['password']) : $old['password']);
-    $stmt->bindParam(':scope', isset($diff['scope']) ? htmlspecialchars(strip_tags($diff['scope'])) : $old['scope']);
-
-    if ($stmt->execute()) {
-      return true;
-    }
-
-    printf("Error: %s.\n", $stmt->error);
-
-    return false;
-  }
-
-  public function deleteOne($id) {
-    $query = 'DELETE FROM ' . $this->table . ' WHERE id = :id';
-    $stmt  = $this->conn->prepare($query);
-    $stmt->bindParam(':id', $id);
-
-    if ($stmt->execute()) {
-      return true;
-    }
-
-    printf("Error: %s.\n", $stmt->error);
-
-    return false;
   }
 
 }
